4ea9d163decc50c5579fb102dc3f45db0fd0850b
Jeremy Felt <jeremy.felt@gmail.com>
2017-06-07 11:02:01 -0700

    Replace skeleton naming in more areas

--- plugin.php
+++ plugin.php
@@ -6,8 +6,7 @@ Description: Embed external HTML components in a style guide or other web docume
 Author: washingtonstateuniversity, jeremyfelt
 Author URI: https://web.wsu.edu/
 Plugin URI: https://github.com/washingtonstateuniversity/WSUWP-Plugin-HTML-Component-Embed
-Text Domain: [Plugin Text Domain]
-Domain Path: /languages
+Text Domain: html-component-embed
 */
 
 // If this file is called directly, abort.
@@ -18,7 +17,7 @@ if ( ! defined( 'WPINC' ) ) {
 // This plugin uses namespaces and requires PHP 5.3 or greater.
 if ( version_compare( PHP_VERSION, '5.3', '<' ) ) {
 	add_action( 'admin_notices', create_function( '',
-	"echo '<div class=\"error\"><p>" . __( 'WSUWP Plugin Skeleton requires PHP 5.3 to function properly. Please upgrade PHP or deactivate the plugin.', 'wsuwp-plugin-skeleton' ) . "</p></div>';" ) );
+	"echo '<div class=\"error\"><p>" . __( 'HTML Component Embed requires PHP 5.3 to function properly. Please upgrade PHP or deactivate the plugin.', 'html-component-embed' ) . "</p></div>';" ) );
 	return;
 } else {
 	include_once __DIR__ . '/includes/html-component-embed.php';
